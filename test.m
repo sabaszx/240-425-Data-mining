3cb1040594195cc3d211180cda95ed4d70f99a42
sabaszx <5910110573@psu.ac.th>
2020-01-06 15:51:19 +0700

    copy from 5dots

--- test.m
+++ test.m
@@ -1,1 +1,7 @@
-fid = fopen('reprocessed_hungarian_data.txt','r')
+fid=fopen('reprocessed_hungarian_data.txt');
+[data, count] = fscanf(fid, '%f %f %f %f %f %f %f %f %f %f %f %f %f %f\n', [14 Inf]);
+size(data)
+count
+
+age=data(1,:)
+fclose(fid);
